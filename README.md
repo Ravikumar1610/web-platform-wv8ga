8e745291ef110b26d23d4eced09863dacea09969
Ravikumar1610 <106589363+Ravikumar1610@users.noreply.github.com>
2023-04-01 13:53:39 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# web-platform-wv8ga
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-wv8gaw)
---
+++ script.js
@@ -0,0 +1,44 @@
+// // console.log('Hello!');
+
+// // const btn=document.getElementById("btn")
+
+// // btn.addEventListener("click",function(){
+
+
+// //  const element=document.createElement("a")
+// //  element.href="www.ssc.com";
+// //  element.target="_blank";
+// //  element.innerText="ssc"
+// //  const div=document.getElementById("append");
+// //  div.appendChild(element)
+// // })
+
+
+// const btn=document.getElementById("app")
+// const array=["sita","rita","gita","rita","mita"]
+
+// btn.addEventListener("click",function(){
+//   const div=document.getElementById("append");
+ 
+//   const elementArray=[];
+//   for( let i=0 ; i<array.length; i++){
+//  const element=document.createElement("li");
+//   element.innerText=array[i];
+  
+//   elementArray.push(element)
+//   }
+//   div.append(...array)
+  
+  
+// })
+
+//CREATE AND IMAGE AND APPEND IT 
+
+const btn=document.getElementById("app");
+
+btn.addEventListener("click",function(){
+
+  const element=document.createElement("img")
+  element.src=""
+})
+/
